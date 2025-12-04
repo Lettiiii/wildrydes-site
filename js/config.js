da0567b7e4f297216f775d2d98d0d1c594e56efa
Lettiiii <108941790+Lettiiii@users.noreply.github.com>
2025-12-04 19:44:45 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,15 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_DqGrRRC0r
+        userPoolId: 'ap-southeast-2_3Y3TP5s5t
 ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1risahm2gkubk5ore95sv6d233', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '79uau7bdsvr25d3igtc3ep8pig', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
